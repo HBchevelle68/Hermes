e6a389d7a1ab4b318c57d0e73f1740d69ac62c0f
HBchevelle68 <aportante10@gmail.com>
2017-06-24 11:35:36 -0700

    fully working now! :)

--- fuzz.c
+++ fuzz.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/udp.h>
 #include <netinet/ip.h>
 #include <net/if.h>
-#include <sys/ioctl.h>
-#include <linux/if_packet.h>
 #include <netdb.h>
+#include <sys/ioctl.h>
+#include <bits/ioctls.h>
+
 
 /*
     The udp checksum is only performed on certain data
@@ -26,14 +29,13 @@ struct udpchk {
 };
 
 void usage();
-unsigned short CheckSum(unsigned short *buffer, int size);
+unsigned short checksum(unsigned short *buffer, int size);
 
 int main(int argc, char* argv[]){
 
     int rsfd, pcktcount;
     const int on = 1;
-    char* temp_csum;
-    char* name = "ens33";
+    char *temp_csum, *ifname, *dst_ip, *src_ip;
     struct sockaddr_in *ipv4, sin;
     struct ip* iph;
     struct udphdr* udph;
@@ -41,21 +43,29 @@ int main(int argc, char* argv[]){
     struct addrinfo hints, *res;
     void* tmp;
 
-    char* dst_ip = malloc(INET_ADDRSTRLEN);
 
-    if(argc < 3) {
+    if(argc < 5) {
         usage();
         return 0;
     }
     
-    if(argv[3][0] == '-' && argv[3][1] == 'c'){
-        pcktcount = atoi(argv[4]);
+    if(argv[4][0] == '-' && argv[4][1] == 'c'){
+        pcktcount = atoi(argv[5]);
     } 
     else {
         usage();
         return 0;
     }
 
+
+    ifname = malloc(20);
+    dst_ip = malloc(INET_ADDRSTRLEN);
+    src_ip = malloc(INET_ADDRSTRLEN);
+
+    strcpy(ifname, argv[1]);
+    strcpy (src_ip, argv[2]);
+    strcpy (dst_ip, argv[3]);
+    
     //create raw socket
     if ((rsfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
         perror("socket() raw socket creation failed ");
@@ -63,27 +73,29 @@ int main(int argc, char* argv[]){
     }
 
     //grab interface index
-    struct ifreq interface;
-    memset(&interface, 0, sizeof(struct ifreq));
-    snprintf (interface.ifr_name, sizeof (interface.ifr_name), "%s", name);
-    if(ioctl(rsfd, SIOCGIFINDEX, &interface) < 0){
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    snprintf (ifr.ifr_name, sizeof(ifr.ifr_name), "%s", ifname);
+    if(ioctl(rsfd, SIOCGIFINDEX, &ifr) < 0){
         perror("IF index ioctl error: ");
         exit(EXIT_FAILURE);
     }
     close(rsfd);
+    printf ("Index for interface %s is %i\n", ifname, ifr.ifr_ifindex);
 
-    // Fill out hints for getaddrinfo().
+
+    // For getaddrinfo().
     memset (&hints, 0, sizeof (struct addrinfo));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = hints.ai_flags | AI_CANONNAME;
 
-    // Resolve target using getaddrinfo().
-    if (getaddrinfo (argv[2], NULL, &hints, &res) != 0) {
-        perror("getaddrinfo() error");
+    // Resolve dst_ip using getaddrinfo().
+    if (getaddrinfo (dst_ip, NULL, &hints, &res) != 0) {
+        perror("getaddrinfo() error: ");
         exit (EXIT_FAILURE);
     }
-    printf("here\n");
+    //convert from binary to text string
     ipv4 = (struct sockaddr_in *) res->ai_addr;
     tmp = &(ipv4->sin_addr);
     if (inet_ntop (AF_INET, tmp, dst_ip, INET_ADDRSTRLEN) == NULL) {
@@ -92,7 +104,6 @@ int main(int argc, char* argv[]){
     }
     freeaddrinfo (res);
 
-    printf("here1\n");
 
     char dgram[IP_MAXPACKET];
     memset(dgram, 0, sizeof(dgram));
@@ -107,13 +118,13 @@ int main(int argc, char* argv[]){
     iph->ip_hl = 5;
     iph->ip_v = IPVERSION;
     iph->ip_tos = 0;
-    iph->ip_len = sizeof(struct ip) + sizeof(struct udphdr) + strlen(data);
+    iph->ip_len = htons(sizeof(struct ip) + sizeof(struct udphdr) + strlen(data));
     iph->ip_id = htonl(0); 
     iph->ip_off = 0;
     iph->ip_ttl = MAXTTL;
     iph->ip_p = IPPROTO_UDP;
     iph->ip_sum = 0;
-    if(inet_pton(AF_INET, argv[1], &(iph->ip_src)) != 1){
+    if(inet_pton(AF_INET, src_ip, &(iph->ip_src)) != 1){
         perror("ip_src inet_pton() error: ");
         exit(EXIT_FAILURE);
     }
@@ -122,12 +133,12 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
     //IP header checksum
-    iph->ip_sum = CheckSum((unsigned short*)&iph, 20);
+    iph->ip_sum = checksum((unsigned short*)&iph, 20);
     
     //Fill in the IP Header
-    udph->source = htons(5555);
+    udph->source = htons(7777);
     udph->dest = htons(5555);
-    udph->len = htons(sizeof(struct udphdr) + strlen(data)); 
+    udph->len = htons(8 + strlen(data)); 
     udph->check = 0;
 
     //begin udp checksum
@@ -147,9 +158,13 @@ int main(int argc, char* argv[]){
     //copy the UDP header after udpchk structure 
     memcpy(temp_csum + sizeof(struct udpchk), udph, sizeof(struct udphdr) + strlen(data));
     //compute checksum and store to UDP headers
-    udph->check = CheckSum((unsigned short*) temp_csum, size);
+    udph->check = checksum((unsigned short*) temp_csum, size);
 
     free(temp_csum);
+
+    memset(&sin, 0, sizeof(struct sockaddr_in));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = iph->ip_dst.s_addr;
 
     //create raw socket
     if ((rsfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
@@ -159,18 +174,15 @@ int main(int argc, char* argv[]){
 
     // Set flag so socket expects IPv4 header.
     if (setsockopt (rsfd, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on)) < 0) {
-        perror ("setsockopt() failed to set IP_HDRINCL ");
+        perror ("setsockopt() failed to set IP_HDRINCL: ");
         exit(EXIT_FAILURE);
     }
 
-    // Bind socket to interface index.
-    if (setsockopt (rsfd, SOL_SOCKET, SO_BINDTODEVICE, &interface, sizeof(interface)) < 0) {
-        perror ("setsockopt() failed to bind to interface ");
+    // Bind socket to ifr index.
+    if (setsockopt (rsfd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof(ifr)) < 0) {
+        perror ("setsockopt() failed to bind to interface: ");
         exit(EXIT_FAILURE);
     }
-
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = inet_addr(argv[1]);
 
     printf("Sending...\n");
 
@@ -180,10 +192,14 @@ int main(int argc, char* argv[]){
             exit(EXIT_FAILURE);
         }
         else {
-            printf("Sent UDP dgram #%d \n", count);
+            printf("\rSent UDP dgram #%d", count);
         }
-        //sleep(1);
+        
     }
+    printf("\n");
+    free(ifname);
+    free(dst_ip);
+    free(src_ip);
 
     close(rsfd);
     
@@ -193,12 +209,14 @@ int main(int argc, char* argv[]){
 
 
 void usage(){
-    printf("Usage: sudo ./fuzz [SRC ADDR] [DEST ADDR] -c [NUM]\n\n");
+    printf("Usage: sudo ./fuzz [INTERFACE] [SRC ADDR] [DEST ADDR] -c [NUM]\n\n");
+    printf("\t[INTERFACE] \tDesired interface to bind to\n");
+    printf("\t[DEST ADDR] \tIPv4 address of source - Can be spoofed :)\n");
     printf("\t[DEST ADDR] \tIPv4 address of target\n");
     printf("\t[NUM] \tNumber of packets to send\n");
 }
 
-unsigned short CheckSum(unsigned short *buffer, int size){
+unsigned short checksum(unsigned short *buffer, int size){
     unsigned long cksum=0;
     while(size >1){
         cksum+=*buffer++;
